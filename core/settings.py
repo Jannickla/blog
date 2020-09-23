ea1221390739415407d7c5303ce7e347e8e99558
jannick.la <jannlang94@gmail.com>
2020-09-23 11:03:44 +0200

    some changes

--- core/settings.py
+++ core/settings.py
@@ -71,8 +71,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        },
     }
 }
 
